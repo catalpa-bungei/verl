1c53f2e756a699c57aa3de88bb904a21b9b29889
catalpa-bungei <catalpabungeiyang@gmail.com>
2025-12-29 11:24:13 +0800

    server 20251229

--- merge_checkpoints.sh
+++ merge_checkpoints.sh
@@ -1,5 +1,5 @@
 export PYTHONPATH=/mnt/shared-storage-user/yangxuqing
 python scripts/model_merger.py merge \
     --backend fsdp \
-    --local_dir /mnt/shared-storage-user/yangxuqing/verl/checkpoints/verl_grpo_text-test0.01_qwen2.5vl-7b_promptv8_T5_temp0.7/text-beta0.1_alpha0.5-20250920/global_step_103/actor \
-    --target_dir /mnt/shared-storage-user/yangxuqing/verl/merged_model/qwen2.5vl-7b_promptv8_T5_temp0.7-text-beta0.1_alpha0.5-20250920/global_step_103 \
+    --local_dir /mnt/shared-storage-user/yangxuqing/verl/checkpoints/verl_grpo_text-test0.01_qwen2.5vl-7b_promptv8_T5_temp0.7-group/text-beta0.5_20251226/global_step_103/actor \
+    --target_dir /mnt/shared-storage-user/yangxuqing/verl/merged_model/qwen2.5vl-7b_promptv8_T5_temp0.7-text-beta0.5-group0.5_20251226/global_step_103 \
